904d029f9ce2604f0931e492a84d3d2ac34c543c
LiGuangQiao <lgqfighting@163.com>
2023-11-07 20:22:32 +0800

    提交git_Tree构建内容

--- src/data/data.rs
+++ src/data/data.rs
@@ -1,16 +1,20 @@
+use crate::data;
 //山楂树下
 use crate::decode;
 use crate::encode;
 use crate::obj_type;
+use anyhow::Error;
 use bstr::ByteSlice;
 use sha1::{Digest, Sha1};
 use std::{fmt, result};
+use rand::{Rng, distributions::Alphanumeric};
 
 struct Data {
     content: String,
     code: String,
     data_type: String,
-    nodes:Option<Vec<Data>>
+    nodes:Option<Vec<Data>>,
+    file_name:String
 }
 impl Data {
     fn new(content: String, code: String, data_type: String) -> Self {
@@ -18,8 +22,27 @@ impl Data {
             content: content,
             code: code,
             data_type: data_type,
-            nodes:None
+            nodes:None,
+            file_name:rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(10)
+            .map(char::from)
+            .collect()
         }
+    }
+    fn new_tree(code: String, data_type: String,nodes:Vec<Data>)->(){
+        let mut content = String::from("");
+        for node in nodes{
+
+            content+=&format!("{} {}\x00{}",&node.data_type.clone(),&node.file_name,hex::decode(&node.get_sha_1()).unwrap());
+        }
+        println!("{:?}",format!("tree {}\x00{}",content.len(),content));
+        // Data {
+        //     content: content,
+        //     code: code,
+        //     data_type: data_type,
+        //     nodes:Some(nodes)
+        // }
     }
     fn get_data(&self) -> String {
         format!(
@@ -35,12 +58,24 @@ impl Data {
         let result = hasher.finalize();
         format!("{:x}", result)
     }
+    fn add_node(&mut self,subnode:Data){
+        if let Some(x) = &mut self.nodes{
+            x.push(subnode);
+        }else{
+            self.nodes = Some(vec![subnode]);
+        }
+    }
     // 树对象的文件内容
     //#{content}
     //mode 40000代表树对象 100644代表blob对象
     // #{mode} #{dir1_name or file1_name}#{dir1 or file1's hash}#{mode} #{dir2_name or file2_name}#{dir2 or file2's hash}...
     // **注意：**`#{dir1 or file1's hash}`需要从`HEX`转换成字节序再作字符串拼接。
-    // fn get_tree_data() -> String {}
+    fn get_tree_data(&self) -> String {
+        fn base_arc(data_type: String,content: String)->String{
+            format!("{} {}\x00{}",data_type,content.len(),content)
+        }
+        String::new()
+    }
 }
 impl fmt::Display for Data {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -94,4 +129,12 @@ fn tree_test() {
     // let result =  encode::zlib_encode(content.as_bytes());
     // println!("{:?}",result.unwrap().as_bstr());
     //rust中\a是1无效转义字符 其对应的响铃字符为\x07,在\a在ruby中是合法的转义字符
+}
+
+#[test]
+fn new_tree_test(){
+    let blob_1 = Data::new("test_one".to_string(),"100064".to_string(),"blob".to_string());
+    let blob_2 = Data::new("test_two".to_string(),"100064".to_string(),"blob".to_string());
+    let nodes = vec![blob_1,blob_2];
+    let tree = Data::new_tree("040000".to_string(),"tree".to_string(),nodes);
 }
